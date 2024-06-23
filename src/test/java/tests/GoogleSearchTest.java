7b7680f1c9b21437dd0e9932dabc9540413433f1
= <=>
2024-06-23 22:42:58 +0530

    Assetion added

--- src/test/java/tests/GoogleSearchTest.java
+++ src/test/java/tests/GoogleSearchTest.java
@@ -4,9 +4,15 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.interactions.KeyInput;
+import org.testng.Assert;
 import util.WebDriverInit;
+
+import java.awt.event.KeyEvent;
 
 public class GoogleSearchTest {
 WebDriver webDriver;
@@ -15,6 +21,7 @@ WebDriver webDriver;
         webDriver = new ChromeDriver();
         webDriver.get("https://www.google.com/");
         webDriver.manage().window().maximize();
+        Assert.assertTrue(true);
     }
 
 
@@ -23,5 +30,14 @@ WebDriver webDriver;
 //        WebDriverInit.webDriver.findElement(By.id("APjFqb")).sendKeys("Automate");
         JavascriptExecutor js = (JavascriptExecutor) webDriver;
         js.executeScript("document.getElementById('APjFqb').value='automate';");
+        Assert.assertTrue(true);
+    }
+
+
+    @Then("Click enter button")
+    public void clickEnterButton() {
+    Actions actions = new Actions(webDriver);
+    actions.sendKeys(Keys.ENTER).build().perform();
+    Assert.assertTrue(true);
     }
 }
--- src/test/resources/features/GoogleSearch.feature
+++ src/test/resources/features/GoogleSearch.feature
@@ -3,3 +3,4 @@ Feature: Sample feature
   Scenario: Sample scenario
     Given I open the Google homepage
     Then enter the input text to Search
+    Then Click enter button
